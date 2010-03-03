b3637ef66752eabcabcc9894a872b2d89c06a3bd
christophe ld <christophe@rouge-rose.net>
2010-03-03 23:37:40 +0100

    suite rubrique video - la colonne extra est calée sur la même verticale que la première vidéo - le formulaire de sélection thématique est supprimé pour utiliser des liens (et ajax) - mise en forme qui va avec

--- squelettes/lang/local_fr.php
+++ squelettes/lang/local_fr.php
@@ -1,13 +1,18 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'nom_prenom' => 'Vos nom et pr&eacute;nom',
     
+    // M
+    'mots_cles' => 'Mots-cl&eacute;s',
+    
+    // N
+    'nom_prenom' => 'Vos nom et pr&eacute;nom',
     
     // S
     'selectionnez' => 'S&eacute;lectionnez',
     
     // T
-    'toutes_videos_disponibles' => 'Toutes les vid&eacute;os disponibles',
+    'toutes_videos' => 'Toutes les vid&eacute;os',
+    'tri_mot_cle' => 'Trier par mot-cl&eacute;',
     'tri_theme' => 'Trier par th&egrave;me'
 );
 ?>
