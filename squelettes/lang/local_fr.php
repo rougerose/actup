e89a823d15ebf1c0287a9883a2f9da15b88fd286
christophe ld <christophe@rouge-rose.net>
2010-03-03 14:13:06 +0100

    tri thématique des vidéos (suite)

--- squelettes/lang/local_fr.php
+++ squelettes/lang/local_fr.php
@@ -1,5 +1,13 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'nom_prenom' => 'Vos nom et pr&eacute;nom'
+    'nom_prenom' => 'Vos nom et pr&eacute;nom',
+    
+    
+    // S
+    'selectionnez' => 'S&eacute;lectionnez',
+    
+    // T
+    'toutes_videos_disponibles' => 'Toutes les vid&eacute;os disponibles',
+    'tri_theme' => 'Trier par th&egrave;me'
 );
 ?>
