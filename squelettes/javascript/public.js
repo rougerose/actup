a92e74db7cbbe3d0e3b1a42269a55e9b3e4966a4
christophe ld <christophe@rouge-rose.net>
2010-03-04 18:45:10 +0100

    page d'accueil - mise en place d'un "slider" basé sur jquery ui

--- squelettes/javascript/public.js
+++ squelettes/javascript/public.js
@@ -5,5 +5,16 @@ Shadowbox.init();
 $(document).ready(function() {
     // grille de mise en page
     $("body").addGrid(16, {img_path: 'squelettes/img/', z_index:'99', left:'0'});
+    
+    // selections sommaire
+    //$('#selection').tabs();
+    $("#selection").tabs({fx:{opacity:'toggle', duration:'slow'}}).tabs('rotate',5000,false);
+    
+    $("#selection li").bind('chgt',function(){
+        $('.ui-tabs-selected').addClass("test");
+    });
+    
+    
+    
 
 });
